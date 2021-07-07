08b9644a7aba1e1501515c4e3984124678afe211
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-07-07 17:57:01 -0300

    init

---
+++ src/main/java/com/study/nanamanager/NanaManagerApplication.java
@@ -0,0 +1,13 @@
+package com.study.nanamanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanaManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NanaManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/nanamanager/NanaManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.nanamanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NanaManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
