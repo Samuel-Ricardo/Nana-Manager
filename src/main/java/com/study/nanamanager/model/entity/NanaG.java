3efdc305f3a3cc9527423425a26f486717485371
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-07-08 15:09:54 -0300

    Created: Entity - NanaG (model.entity)

--- src/main/java/com/study/nanamanager/model/entity/NanaG.java
+++ src/main/java/com/study/nanamanager/model/entity/NanaG.java
@@ -1,14 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.study.nanamanager.model.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
 
 /**
  *
  * @author Samuel
  */
+
+//Packing - embalagem + ml
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class NanaG {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private NanaType type;
+    
+    private NanaPacking packing;
+    
+    private Nutrients nutrients;
+    
+    private String Ingredients;
+    
+    private Long stock;
 }
