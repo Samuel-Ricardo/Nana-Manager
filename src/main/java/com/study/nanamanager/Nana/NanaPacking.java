9ccccafdc178c5c6158c51a5f18e42be29e8ba43
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-07-08 16:13:56 -0300

    Added: Class - NanaPacking (Nana)

---
+++ src/main/java/com/study/nanamanager/Nana/NanaPacking.java
@@ -0,0 +1,20 @@
+package com.study.nanamanager.Nana;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author Samuel
+ */
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class NanaPacking {
+    
+    private String pack;
+    private String ml;
+    
+}
