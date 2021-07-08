824ea27edb1ee46f6c4456bf2645e69cafc411c9
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-07-08 18:31:11 -0300

    Created: DTO - NanaDTO - (dto)

--- src/main/java/com/study/nanamanager/dao/service/NanaService.java
+++ src/main/java/com/study/nanamanager/dao/service/NanaService.java
@@ -1,11 +1,19 @@
 package com.study.nanamanager.dao.service;
 
+import com.study.nanamanager.dao.repository.NanaRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 /**
  *
  * @author Samuel
  */
 
-
+@Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class NanaService {
     
+    private final NanaRepository nanaRepository;
+//    private final NanaMapper nanaMapper;
 }
---
+++ src/main/java/com/study/nanamanager/dto/request/NanaDTO.java
@@ -0,0 +1,53 @@
+package com.study.nanamanager.dto.request;
+
+import com.study.nanamanager.Nana.NanaPacking;
+import com.study.nanamanager.Nana.NanaType;
+import com.study.nanamanager.Nana.Nutrients;
+import com.study.nanamanager.Nana.Store;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author Samuel
+ */
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class NanaDTO {
+    
+    private Long id;
+    
+    @NotNull
+    @Size(min = 1, max =  200)
+    private String name;
+    
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private NanaType nanaType;
+    
+    @NotNull
+    private NanaPacking packing;
+    
+    @NotNull
+    private Nutrients nutrients;
+    
+    @Column (nullable = false)
+    private Long max;
+    
+    @NotNull
+    private Long stock;
+    
+    @NotNull
+    private List<Store> stores;
+}
