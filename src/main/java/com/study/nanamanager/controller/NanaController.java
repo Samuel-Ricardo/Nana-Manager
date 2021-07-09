62bf9fe0d9b95c34c95d3f7d1cf4dc96b691a700
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-07-09 15:40:12 -0300

    Implements: Interface - NanaController (controller)

--- src/main/java/com/study/nanamanager/controller/NanaController.java
+++ src/main/java/com/study/nanamanager/controller/NanaController.java
@@ -5,6 +5,12 @@
  */
 package com.study.nanamanager.controller;
 
+import com.study.nanamanager.controller.documentation.NanaControllerDoc;
+import com.study.nanamanager.dto.request.NanaDTO;
+import com.study.nanamanager.dto.response.Response;
+import com.study.nanamanager.exceptions.NanaAlreadyRegisteredException;
+import com.study.nanamanager.exceptions.NanaNotFoundException;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +25,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/nana-manager")
 //@RequestMapping("/api/v1/nana")
 @AllArgsConstructor(onConstructor = @__(@Autowired))
-public class NanaController {
+public class NanaController implements NanaControllerDoc{
+
+    @Override
+    public Response<NanaDTO, NanaAlreadyRegisteredException> createNana(NanaDTO nanaDTO) throws NanaAlreadyRegisteredException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Response<NanaDTO, NanaNotFoundException> findByName(String name) throws NanaNotFoundException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Response<NanaDTO, Exception>> listBeers() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Response<NanaDTO, NanaNotFoundException> deleteById(Long id) throws NanaNotFoundException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
