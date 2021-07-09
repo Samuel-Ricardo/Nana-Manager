f14b3b430db7c7498e7c7823094cf1ce89a6f45a
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-07-09 15:58:47 -0300

    Created: Function - checkIfExistsNana(String) (NanaService)

--- src/main/java/com/study/nanamanager/dao/service/NanaService.java
+++ src/main/java/com/study/nanamanager/dao/service/NanaService.java
@@ -1,6 +1,12 @@
 package com.study.nanamanager.dao.service;
 
 import com.study.nanamanager.dao.repository.NanaRepository;
+import com.study.nanamanager.dto.mapper.NanaMapper;
+import com.study.nanamanager.dto.request.NanaDTO;
+import com.study.nanamanager.dto.response.Response;
+import com.study.nanamanager.exceptions.NanaAlreadyRegisteredException;
+import com.study.nanamanager.model.entity.NanaG;
+import java.util.Optional;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,5 +21,23 @@ import org.springframework.stereotype.Service;
 public class NanaService {
     
     private final NanaRepository nanaRepository;
-//    private final NanaMapper nanaMapper;
+    private final NanaMapper nanaMapper = NanaMapper.INSTANCE;
+
+    public Response<NanaDTO, NanaAlreadyRegisteredException> createNana(NanaDTO nanaDTO) {
+        
+        Optional<NanaG> nana = getNana(nanaDTO.getName());
+    
+        if(nana.isPresent()) {
+            
+        }
+        
+        return null;
+    }
+
+    private boolean checkIfExistsNana(String name) {
+        Optional<NanaG> optSavedNana = nanaRepository.findByName(name);
+        
+        return optSavedNana.isPresent();
+    }
+
 }
