23eee84ea2ce2143b3139d92f1c6d6ef4c5aa6bc
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-07-10 18:05:06 -0300

    create: Method - findByName(String name) - NanaService - (dao.service)

--- src/main/java/com/study/nanamanager/dao/service/NanaService.java
+++ src/main/java/com/study/nanamanager/dao/service/NanaService.java
@@ -5,6 +5,7 @@ import com.study.nanamanager.dto.mapper.NanaMapper;
 import com.study.nanamanager.dto.request.NanaDTO;
 import com.study.nanamanager.dto.response.Response;
 import com.study.nanamanager.exceptions.NanaAlreadyRegisteredException;
+import com.study.nanamanager.exceptions.NanaNotFoundException;
 import com.study.nanamanager.model.entity.NanaG;
 import java.util.Optional;
 import lombok.AllArgsConstructor;
@@ -16,51 +17,55 @@ import org.springframework.stereotype.Service;
  *
  * @author Samuel
  */
-
 @Service
 //@AllArgsConstructor()
 public class NanaService {
-    
+
     private final NanaRepository nanaRepository;
     private final NanaMapper nanaMapper = NanaMapper.INSTANCE;
 
+    @Autowired
     public NanaService(NanaRepository nanaRepository) {
         this.nanaRepository = nanaRepository;
     }
-    
-    
-    
+
     public Response<NanaDTO> createNana(NanaDTO nanaDTO) throws NanaAlreadyRegisteredException {
-    
+
 //        Optional<String> error = error.ofNullable("");
-        
-        if(checkIfExistsNana(nanaDTO.getName()) == true) {
-            
+        if (checkIfExistsNana(nanaDTO.getName()) == true) {
+
             throw new NanaAlreadyRegisteredException(nanaDTO.getName());
         }
-        
+
         NanaG nana = nanaMapper.toModel(nanaDTO);
         NanaG savedNana = nanaRepository.save(nana);
         return new Response<NanaDTO>(
                 nanaMapper.toDTO(savedNana),
-                "", 
+                "Created With Success",
                 HttpStatus.CREATED);
+    }
+
+    public Response<NanaDTO> findByName(String name) throws NanaNotFoundException {
+        NanaG foundNana = nanaRepository.findByName(name)
+                .orElseThrow(() -> new NanaNotFoundException(name));
+        
+        return new Response<NanaDTO>(
+                nanaMapper.toDTO(foundNana), 
+                "Nana Found With Success", 
+                HttpStatus.FOUND);
     }
 
     private boolean checkIfExistsNana(String name) {
         Optional<NanaG> optSavedNana = nanaRepository.findByName(name);
-        
+
         return optSavedNana.isPresent();
     }
-    
+
     private Optional<NanaG> getNana(String name) {
         Optional<NanaG> optSavedNana = nanaRepository.findByName(name);
-        
+
         return optSavedNana;
     }
-    
-    
-    
 
     public NanaRepository getNanaRepository() {
         return nanaRepository;
