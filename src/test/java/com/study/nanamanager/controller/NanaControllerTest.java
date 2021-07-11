d3b3e3f17e690422d5448c2f51aff476a1e8fe8a
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-07-11 16:04:26 -0300

    create: BeforeEach - setUp - (test,controller)

--- src/test/java/com/study/nanamanager/controller/NanaControllerTest.java
+++ src/test/java/com/study/nanamanager/controller/NanaControllerTest.java
@@ -6,33 +6,43 @@
 package com.study.nanamanager.controller;
 
 import com.study.nanamanager.dao.service.NanaService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 /**
  *
  * @author Samuel
  */
-
 @ExtendWith(MockitoExtension.class)
 public class NanaControllerTest {
-    
-    private static final String DEFAULT_URL = "/api/v1/nana";         
-    private static final long VALID_NANA_ID = 1L;         
+
+    private static final String DEFAULT_URL = "/api/v1/nana";
+    private static final long VALID_NANA_ID = 1L;
     private static final long INVALID_NANA_ID = 2L;
 
     private MockMvc mockMvc;
-    
+
     @Mock
     private NanaService service;
-    
+
     @InjectMocks
     private NanaController controller;
-    
+
+    @BeforeEach
     void setUp() {
-        
+
+        mockMvc = MockMvcBuilders.standaloneSetup(controller)
+                .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver())
+                .setViewResolvers((s, locale) -> new MappingJackson2JsonView())
+                .build();
+    
     }
 }
