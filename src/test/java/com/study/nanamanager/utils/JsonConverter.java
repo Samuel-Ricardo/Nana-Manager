a039473bf79f868e9dc4199cc4ae82957f1bef80
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-07-11 14:16:11 -0300

    create: Function - toJsonString() - (JsonConverter)

--- src/test/java/com/study/nanamanager/utils/JsonConverter.java
+++ src/test/java/com/study/nanamanager/utils/JsonConverter.java
@@ -5,10 +5,35 @@
  */
 package com.study.nanamanager.utils;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.study.nanamanager.dto.request.NanaDTO;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Samuel
  */
 public class JsonConverter {
     
+    public static String toJsonString(Object nanaDTO) {
+        
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            
+            mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+            mapper.registerModule(new JavaTimeModule());
+            
+            return mapper.writeValueAsString(nanaDTO);
+        } catch (JsonProcessingException ex) {
+            Logger.getLogger(JsonConverter.class.getName()).log(Level.SEVERE, null, ex);
+        
+            throw new RuntimeException(ex);
+        }
+    }
 }
